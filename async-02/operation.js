935ad6d9f638bc21a07f87cc0d75d9e21ce572c9
dtrishkin <dtrishkin@mirantis.com>
2016-07-18 19:11:17 +0300

    Asynchronous Programming lesson 2 complete. Coordinating Parallel Calls.

---
+++ async-02/operation.js
@@ -0,0 +1,57 @@
+module.exports = function composedCall(args, cb){
+
+  var pending = 0;
+  var results = [];
+  var calledback = false;
+
+  call1(args, handleResult());
+  call2(args, handleResult());
+  call3(args, handleResult());
+
+  function handleResult() {
+    var order = pending;
+    pending++;
+    return function(err, result) {
+      pending--;
+      if (err) {
+        callback(err);
+      } else {
+        results[order] = result;
+        if (!pending) {
+          callback(null, results);
+        }
+      }
+    };
+  }
+
+  function callback(err, value) {
+    if (!calledback) {
+      calledback = true;
+      cb(err, value);
+    }
+  }
+};
+
+// Calls
+
+function call1(args, cb) {
+  setTimeout(cb, randomTimeout(), null, randomValue());
+}
+
+function call2(args, cb) {
+  setTimeout(cb, randomTimeout(), null, randomValue());
+}
+
+function call3(args, cb) {
+  setTimeout(cb, randomTimeout(), null, randomValue());
+}
+
+// Utils
+
+function randomTimeout() {
+  return Math.floor(Math.random() * 1e3);
+}
+
+function randomValue() {
+  return Math.floor(Math.random() * 1e10);
+}
